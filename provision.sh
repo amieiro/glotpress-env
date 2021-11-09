c67921bf7c6ffd82e0b63c52c32bea46f37cbb55
Jesús Amieiro <amieiro@gmail.com>
2021-11-09 17:22:57 +0100

    Remove some useless items

--- provision.sh
+++ provision.sh
@@ -4,7 +4,3 @@
 
 # We need a checkout of meta
 [[ -d glotpress.git ]] || git clone https://github.com/GlotPress/GlotPress-WP.git glotpress.git
-[[ -d meta.git ]] || git clone https://github.com/wordpress/wordpress.org meta.git
-
-# We need a checkout of theme-check
-[[ -d wp-content/plugins/theme-check ]] || git clone https://github.com/wordpress/theme-check wp-content/plugins/theme-check
